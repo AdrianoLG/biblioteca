e50aee62c3432c71fea61cdadb622f664f48a55a
AdrianoLG <adri@adri.info>
2016-03-03 22:20:56 +0100

    Primer index 	new file:   index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Biblioteca</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<section>
+		<form action="index.php" method="POST">
+			<label for="titulo">Título</label>
+			<input type="text" name="titulo" id="titulo" maxlength="100">
+			<label for="autor">Autor</label>
+			<input type="text" name="autor" id="autor" maxlength="100">
+		</form>
+	</section>
+</body>
+</html>
